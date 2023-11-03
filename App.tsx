4976a9943ae947fccea3ab7d9396b3c518ab85c3
itsnyx <xitsnyx@gmail.com>
2023-11-03 10:22:25 +0330

    created repo

---
+++ App.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+import {
+  createWeb3Modal,
+  defaultWagmiConfig,
+  Web3Modal,
+  W3mButton,
+} from '@web3modal/wagmi-react-native';
+import {FlexView, Text} from '@web3modal/ui-react-native';
+import Clipboard from '@react-native-clipboard/clipboard';
+
+import {WagmiConfig} from 'wagmi';
+import {
+  arbitrum,
+  mainnet,
+  polygon,
+  avalanche,
+  bsc,
+  optimism,
+  gnosis,
+  zkSync,
+  zora,
+  base,
+  celo,
+  aurora,
+} from 'wagmi/chains';
+
+// 1. Get projectId
+const projectId = 'PROJECT ID';
+
+// 2. Create config
+const metadata = {
+  name: 'Web3Modal + wagmi',
+  description: 'Web3Modal + wagmi',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+  redirect: {
+    native: 'w3mwagmisample://',
+  },
+};
+
+const clipboardClient = {
+  setString: async (value: string) => {
+    Clipboard.setString(value);
+  },
+};
+
+const chains = [
+  mainnet,
+  polygon,
+  avalanche,
+  arbitrum,
+  bsc,
+  optimism,
+  gnosis,
+  zkSync,
+  zora,
+  base,
+  celo,
+  aurora,
+];
+
+const wagmiConfig = defaultWagmiConfig({chains, projectId, metadata});
+
+// 3. Create modal
+createWeb3Modal({
+  projectId,
+  chains,
+  wagmiConfig,
+  clipboardClient,
+});
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <SafeAreaView style={[styles.container, isDarkMode && styles.dark]}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <Text style={styles.title} variant="large-600">
+          Web3Modal + wagmi
+        </Text>
+        <FlexView gap="xs">
+          <W3mButton balance="show" />
+        </FlexView>
+        <Web3Modal />
+      </SafeAreaView>
+    </WagmiConfig>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  dark: {
+    backgroundColor: '#141414',
+  },
+  title: {
+    marginBottom: 40,
+    fontSize: 30,
+  },
+});
+
+export default App;
